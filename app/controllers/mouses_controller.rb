3f0f6c9f32d100ba964e51d92254cc64fb307e13
JRisJunior <jeffmrubin@gmail.com>
2023-01-17 11:39:42 -0600

    add create action and route

--- app/controllers/mouses_controller.rb
+++ app/controllers/mouses_controller.rb
@@ -3,5 +3,14 @@ class MousesController < ApplicationController
   def index
     mouses = Mouse.all
     render json: mouses.as_json
-  end 
+  end
+
+  def create
+    mouse = Mouse.save(
+      brand: params[:brand],
+      model: params[:model],
+      dpi: params[:dpi]
+    )
+    render json: {message: "hello from create"}
+  end
 end
